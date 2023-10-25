d8425257f6b60ea68ee99716a524df8d12bf23b5
Raji G <rajith0417@gmail.com>
2023-10-25 16:18:13 +0530

    feat: scroll auto load function implemented

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import "./App.scss";
 import CustomButton from "./components/CustomButton";
 import { CharacterList } from "./components";
 import { Character } from "./types";
+import throttle from "lodash/throttle";
+import { debounce } from "lodash";
 
 function App() {
   const [characters, setCharacters] = useState<Character[]>([]);
@@ -12,6 +14,7 @@ function App() {
   const [favoriteCharacterIds, setFavoriteCharacterIds] = useState<number[]>(
     []
   );
+  const [pageCount, setPageCount] = useState<number>(1);
 
   const filteredCharacters = displayFavorites
     ? characters.filter((character) =>
@@ -21,34 +24,64 @@ function App() {
 
   useEffect(() => {
     //get characters from api
-    const api = async () => {
-      try {
-        setLoading(true);
-        const data = await fetch("https://rickandmortyapi.com/api/character", {
-          method: "GET",
-        });
-        if (!data.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const CharacterData = await data.json();
-        setCharacters(CharacterData.results);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    api();
+    api(pageCount);
 
     // Check localStorage for favorite character IDs on page load
     const storageFavoriteIds = JSON.parse(
       localStorage.getItem("favoriteCharacterIds") || "[]"
     );
     setFavoriteCharacterIds(storageFavoriteIds);
-  }, []);
 
-  // function toggleFavorite(character: Character): void {
+    // Attach the event listener when the component mounts
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      console.log("at bottom unmount-----");
+    };
+  }, [pageCount]);
+
+  const api = async (count: number) => {
+    try {
+      console.log("api---");
+
+      setLoading(true);
+      const data = await fetch(
+        `https://rickandmortyapi.com/api/character?page=${count}`,
+        {
+          method: "GET",
+        }
+      );
+      if (!data.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const CharacterData = await data.json();
+      setCharacters([...characters, ...CharacterData.results]);
+      console.log(CharacterData.results);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleScroll = debounce(() => {
+    console.log("throttle");
+    const windowHeight = window.innerHeight;
+    const scrollPosition = window.scrollY;
+    const documentHeight = document.documentElement.scrollHeight;
+
+    // Check if the user has scrolled to the bottom
+    if (scrollPosition + windowHeight >= documentHeight) {
+      console.log("at bottom-----"+pageCount);
+      setPageCount((prevPageCount) => prevPageCount + 1);
+    }
+  }, 400);
+
   function toggleFavorite(characterId: number): void {
+    console.log("fav cara id - "+characterId);
+    
     if (favoriteCharacterIds.includes(characterId)) {
       setFavoriteCharacterIds(
         favoriteCharacterIds.filter(
@@ -83,12 +116,12 @@ function App() {
         />
       </header>
       <section className="card-wrapper">
-        {loading && <div className="loading">Loading...</div>}
         <CharacterList
           characters={filteredCharacters}
           onToggleFavorite={toggleFavorite}
           favoriteCharacterIds={favoriteCharacterIds}
         />
+        {loading && <div className="loading">Loading...</div>}
       </section>
     </div>
   );
